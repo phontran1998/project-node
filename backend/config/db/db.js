322a5ea062dc934efeb407661021e2ec0853980d
phontran1998 <sinhthaidang310800@gmail.com>
2022-01-06 18:48:25 +0700

    Final

--- backend/config/db/db.js
+++ backend/config/db/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.u9ydf.mongodb.net/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
 const connect = async () => {
   try {
-    await mongoose.connect('mongodb://localhost/SP_Cart', {
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
     });
     console.log('Connect Successfully Demo Ne');
